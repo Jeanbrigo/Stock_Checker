d877fad1874e63fc8a9065637aeb2190a13f4156
Jean Brigonnet <jeanbrigo@gmail.com>
2022-10-23 11:05:44 -0500

    added trending func/need to make 1 call

--- app.js
+++ app.js
@@ -1,49 +1,41 @@
 const apikey = "IJNV4P2J1BEX9AZ5"
 
-
-
 'use strict';
 
 
 var baseURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY` // URL for basic price return pull
 var baseURL2 = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&&outputsize=compact&apikey=${apikey}`; // URL for intraday info
-var baseURL3 = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=${apikey}` // for weekly series, for trending tool purpose.
+var baseURL3 = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY` // for weekly series, for trending tool purpose.
 
-console.log(baseURL3)
+
+// Stock Price Tool
 
 function stockChecker(stock){
     // constructing url for request
     const url = `${baseURL}&symbol=${stock}&apikey=${apikey}`
 
-    
+    const request = $.ajax(url)
 
-
-
-
-const request = $.ajax(url)
-
-request.then((stock) => {
+    request.then((stock) => {
 
     const lastInput = stock["Meta Data"]["3. Last Refreshed"] // Get the date of the most current report
 
-   // Get the Date of the last week posting
+    // console.log(lastInput)
 
-//     const today = new Date()  // Obtained from https://flaviocopes.com/how-to-get-lastWeek-date-javascript/
-// const lastWeek = new Date(today)
 
-//  lastWeek.setDate(lastWeek.getDate()- 7)
+    // get the date of the last week reporting.
 
-// today.toDateString()
-// lastWeek.toDateString()
 
-// const lastWeekInput = new Date(lastInput.getDate() - 7).toISOString().slice(0, 10)
+    const lastWeekInput = new Date(lastInput); // get the date of the report one week from last one
+    let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
+    lastWeek = new Date(lastWeek).toISOString().slice(0, 10);
 
-// console.log(lastWeekInput)
+    // console.log(lastWeek)
 
-// console.log(stock["Weekly Time Series"][lastWeekInput]["4.close"])
+    // console.log(stock["Weekly Time Series"][lastWeek]["4.close"])
 
-    console.log(stock)
-    console.log(stock["Time Series (Daily)"][lastInput]["4. close"])
+    //console.log(stock)
+    //console.log(stock["Time Series (Daily)"][lastInput]["4. close"])
 
     const $main = $("main")
         $main.empty()
@@ -52,6 +44,54 @@ request.then((stock) => {
         <h2>Current price: ${stock["Time Series (Daily)"][lastInput]["4. close"]}</h2>
         `)
 
+
+})
+}
+
+
+// Trend tool
+
+function stockTrender(stock){
+    // constructing url for request
+    const url = `${baseURL3}&symbol=${stock}&apikey=${apikey}`
+
+    const request = $.ajax(url)
+
+    request.then((stock) => {
+
+    const lastInput = stock["Meta Data"]["3. Last Refreshed"] // Get the date of the most current report
+
+    console.log(lastInput)
+
+    // get the date of the last week reporting.
+
+    const lastWeekInput = new Date(lastInput);
+    let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
+    lastWeek = new Date(lastWeek).toISOString().slice(0, 10);
+
+    console.log(lastWeek)
+
+    console.log(stock["Weekly Time Series"][lastWeek]["4. close"])
+
+    console.log(stock)
+    console.log(stock["Weekly Time Series"][lastInput]["4. close"])
+
+
+    if(stock["Weekly Time Series"][lastInput]["4. close"] > stock["Weekly Time Series"][lastWeek]["4. close"]){ 
+    const $main = $("main")
+        //$main.empty()
+        $main.append(`
+        <h1>Trending Upwards</h1>
+        `)}
+    else if(stock["Weekly Time Series"][lastInput]["4. close"] < stock["Weekly Time Series"][lastWeek]["4. close"]) { 
+        const $main = $("main")
+            $main.append(`
+            <h1>Trending Downwards</h1>
+            `)}
+     else{
+        const $main = $("main")
+            $main.append(`<h1>Trending Same as Previous</h1>`)
+     }   
 
 })
 }
@@ -70,17 +110,5 @@ $("input[type=submit]").on("click", (event) => {
 
     // update the screen
     stockChecker(inputText)
+    stockTrender(inputText)
 })
-
-
-// const today = new Date()  // Obtained from https://flaviocopes.com/how-to-get-lastWeek-date-javascript/
-// const lastWeek = new Date(today)
-
-//  lastWeek.setDate(lastWeek.getDate()- 1)
-
-// today.toDateString()
-// lastWeek.toDateString()
-
-// const lastInput = lastWeek.toISOString().slice(0, 10)
-
-// console.log(lastInput)
