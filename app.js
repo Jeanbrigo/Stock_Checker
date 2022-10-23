c9d1afc9f4ecc08a95a12bcaa4bec579f0fda00b
Jean Brigonnet <jeanbrigo@gmail.com>
2022-10-23 18:01:59 -0500

    added hover data

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const apikey = "IJNV4P2J1BEX9AZ5"
 
+// when hover over stock price, gives all stock info for the day
+// when hover trending gives price for last 4 weeks, if price was higher show green if lower show red
+
 'use strict';
 
-
 var baseURL = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY` // URL for basic price return pull
-var baseURL2 = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&&outputsize=compact&apikey=${apikey}`; // URL for intraday info
-var baseURL3 = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY` // for weekly series, for trending tool purpose.
 
 
 // Stock Price Tool
@@ -22,7 +22,7 @@ function stockChecker(stock){
     // console.log(lastInput)
 
 
-    // get the date of the last weeks reporting.
+    // get the date of the last weeks reporting. original code source: http://bluegalaxy.info/codewalk/2018/04/27/javascript-add-subtract-days-iso-date-time-strings/
 
     const lastWeekInput = new Date(lastInput); // get the date of the report one week from last one
     let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
@@ -64,88 +64,50 @@ function stockChecker(stock){
 
 
     // average price for the last month trender
-    const trendMonthPrice = (lastWeekPrice + last2WeekPrice + last3WeekPrice + last4WeekPrice) / 4
+    const trendMonthPrice = (lastWeekPrice + last2WeekPrice + last3WeekPrice + last4WeekPrice) / 4;
+    // console.log(trendMonthPrice)
 
-    console.log(trendMonthPrice)
+
+    const latestDayInfo = JSON.stringify(stock["Time Series (Daily)"][lastInput]);
+console.log(latestDayInfo)
+    const latestDayInfoClean = latestDayInfo.replace(/"/g," ")
 
     // console.log(stock["Weekly Time Series"][lastWeek]["4.close"])
 
-    //console.log(stock)
-    //console.log(stock["Time Series (Daily)"][lastInput]["4. close"])
+    console.log(stock)
+    // console.log(stock["Time Series (Daily)"][lastInput]["4. close"])
 
     const $main = $("main")
         $main.empty()
         $main.html(`
         <h1>${stock["Meta Data"]["2. Symbol"]}</h1>
-        <h2>Current price: $${lastPrice}</h2>
+        <h2 id="price"> Current Price: $${lastPrice}</h2>
+        <h5 id="hideInfo">Stock Info: ${latestDayInfoClean}</h5>
         `)
         if(trendMonthPrice < lastPrice){ 
             const $main = $("main")
                 //$main.empty()
                 $main.append(`
-                <h2>Trending <span style="color: green">Upwards</span></h2>
+                <h2 id="trending">Trending <span style="color: green">Upwards</span></h2>
                 `)}
         else if(trendMonthPrice > lastPrice) { 
             const $main = $("main")
                 $main.append(`
-                <h2>Trending <span style="color: red">Downwards</span></h2>
+                <h2 id="trending">Trending <span style="color: red">Downwards</span></h2>
                 `)}
         else{
             const $main = $("main")
-                $main.append(`<h2>Trending Same as Previous</h2>`)
+                $main.append(`<h2 id="trending">Trending Same as Previous</h2>`)
             }   
-
+        $main.append(`
+        <h5 id=fourWeeks> 4 Weeks Ago: $${last4WeekPrice} /
+        3 Weeks Ago: $${last3WeekPrice} /
+        2 Weeks Ago: $${last2WeekPrice} /
+        1 Week Ago: $${lastWeekPrice}</h5>`
+        )
 })
-}
-
-
-// Trend tool
-
-// function stockTrender(stock){
-//     // constructing url for request
-//     const url = `${baseURL3}&symbol=${stock}&apikey=${apikey}`
-
-//     const request = $.ajax(url)
-
-//     request.then((stock) => {
-
-//     const lastInput = stock["Meta Data"]["3. Last Refreshed"] // Get the date of the most current report
-
-//     console.log(lastInput)
-
-//     // get the date of the last week reporting.
-
-//     const lastWeekInput = new Date(lastInput);
-//     let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
-//     lastWeek = new Date(lastWeek).toISOString().slice(0, 10);
-
-//     console.log(lastWeek)
-
-//     console.log(stock["Weekly Time Series"][lastWeek]["4. close"])
-
-//     console.log(stock)
-//     console.log(stock["Weekly Time Series"][lastInput]["4. close"])
-
-//     // trend function 
-//     if(stock["Weekly Time Series"][lastInput]["4. close"] > stock["Weekly Time Series"][lastWeek]["4. close"]){ 
-//     const $main = $("main")
-//         //$main.empty()
-//         $main.append(`
-//         <h2>Trending Upwards</h2>
-//         `)}
-//     else if(stock["Weekly Time Series"][lastInput]["4. close"] < stock["Weekly Time Series"][lastWeek]["4. close"]) { 
-//         const $main = $("main")
-//             $main.append(`
-//             <h2>Trending Downwards</h2>
-//             `)}
-//      else{
-//         const $main = $("main")
-//             $main.append(`<h2>Trending Same as Previous</h2>`)
-//      }   
-
-// })
-// }
-
+.catch(err=>console.log(err)) 
+};
 
 
 // Submission operators
@@ -160,5 +122,4 @@ $("input[type=submit]").on("click", (event) => {
 
     // update the screen
     stockChecker(inputText)
-   // stockTrender(inputText)
-})
+});
