c24394206426de702c535e4e09cc5a8ccd06eb68
Jean Brigonnet <jeanbrigo@gmail.com>
2022-10-23 12:24:43 -0500

    made all functions under 1 call instead of 2

--- app.js
+++ app.js
@@ -19,18 +19,54 @@ function stockChecker(stock){
     request.then((stock) => {
 
     const lastInput = stock["Meta Data"]["3. Last Refreshed"] // Get the date of the most current report
-
     // console.log(lastInput)
 
 
-    // get the date of the last week reporting.
-
+    // get the date of the last weeks reporting.
 
     const lastWeekInput = new Date(lastInput); // get the date of the report one week from last one
     let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
     lastWeek = new Date(lastWeek).toISOString().slice(0, 10);
 
+    const last2WeekInput = new Date(lastInput); // get the date of the report one week from last one
+    let last2Week = last2WeekInput.setDate(last2WeekInput.getDate() - 14);
+    last2Week = new Date(last2Week).toISOString().slice(0, 10);
+
+    const last3WeekInput = new Date(lastInput); // get the date of the report one week from last one
+    let last3Week = last3WeekInput.setDate(last3WeekInput.getDate() - 21);
+    last3Week = new Date(last3Week).toISOString().slice(0, 10);
+
+    const last4WeekInput = new Date(lastInput); // get the date of the report one week from last one
+    let last4Week = last4WeekInput.setDate(last4WeekInput.getDate() - 28);
+    last4Week = new Date(last4Week).toISOString().slice(0, 10);
+
     // console.log(lastWeek)
+    // console.log(last2Week)
+    // console.log(last3Week)
+    // console.log(last4Week)
+
+    // Price returns (Last price, Last week price, last 2 week price...)
+
+    const lastPrice = parseFloat(stock["Time Series (Daily)"][lastInput]["4. close"]);
+    console.log(lastPrice);
+
+    const lastWeekPrice = parseFloat(stock["Time Series (Daily)"][lastWeek]["4. close"]);
+    console.log(lastWeekPrice);
+
+    const last2WeekPrice = parseFloat(stock["Time Series (Daily)"][last2Week]["4. close"]);
+    console.log(last2WeekPrice);
+
+    const last3WeekPrice = parseFloat(stock["Time Series (Daily)"][last3Week]["4. close"]);
+    console.log(last3WeekPrice);
+
+    const last4WeekPrice = parseFloat(stock["Time Series (Daily)"][last4Week]["4. close"]);
+    console.log(last4WeekPrice);
+
+
+    // average price for the last month trender
+    const trendMonthPrice = (lastWeekPrice + last2WeekPrice + last3WeekPrice + last4WeekPrice) / 4
+
+    console.log(trendMonthPrice)
 
     // console.log(stock["Weekly Time Series"][lastWeek]["4.close"])
 
@@ -41,9 +77,23 @@ function stockChecker(stock){
         $main.empty()
         $main.html(`
         <h1>${stock["Meta Data"]["2. Symbol"]}</h1>
-        <h2>Current price: ${stock["Time Series (Daily)"][lastInput]["4. close"]}</h2>
+        <h2>Current price: $${lastPrice}</h2>
         `)
-
+        if(trendMonthPrice < lastPrice){ 
+            const $main = $("main")
+                //$main.empty()
+                $main.append(`
+                <h2>Trending <span style="color: green">Upwards</span></h2>
+                `)}
+        else if(trendMonthPrice > lastPrice) { 
+            const $main = $("main")
+                $main.append(`
+                <h2>Trending <span style="color: red">Downwards</span></h2>
+                `)}
+        else{
+            const $main = $("main")
+                $main.append(`<h2>Trending Same as Previous</h2>`)
+            }   
 
 })
 }
@@ -51,50 +101,50 @@ function stockChecker(stock){
 
 // Trend tool
 
-function stockTrender(stock){
-    // constructing url for request
-    const url = `${baseURL3}&symbol=${stock}&apikey=${apikey}`
+// function stockTrender(stock){
+//     // constructing url for request
+//     const url = `${baseURL3}&symbol=${stock}&apikey=${apikey}`
 
-    const request = $.ajax(url)
+//     const request = $.ajax(url)
 
-    request.then((stock) => {
+//     request.then((stock) => {
 
-    const lastInput = stock["Meta Data"]["3. Last Refreshed"] // Get the date of the most current report
+//     const lastInput = stock["Meta Data"]["3. Last Refreshed"] // Get the date of the most current report
 
-    console.log(lastInput)
+//     console.log(lastInput)
 
-    // get the date of the last week reporting.
+//     // get the date of the last week reporting.
 
-    const lastWeekInput = new Date(lastInput);
-    let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
-    lastWeek = new Date(lastWeek).toISOString().slice(0, 10);
+//     const lastWeekInput = new Date(lastInput);
+//     let lastWeek = lastWeekInput.setDate(lastWeekInput.getDate() - 7);
+//     lastWeek = new Date(lastWeek).toISOString().slice(0, 10);
 
-    console.log(lastWeek)
+//     console.log(lastWeek)
 
-    console.log(stock["Weekly Time Series"][lastWeek]["4. close"])
+//     console.log(stock["Weekly Time Series"][lastWeek]["4. close"])
 
-    console.log(stock)
-    console.log(stock["Weekly Time Series"][lastInput]["4. close"])
+//     console.log(stock)
+//     console.log(stock["Weekly Time Series"][lastInput]["4. close"])
 
+//     // trend function 
+//     if(stock["Weekly Time Series"][lastInput]["4. close"] > stock["Weekly Time Series"][lastWeek]["4. close"]){ 
+//     const $main = $("main")
+//         //$main.empty()
+//         $main.append(`
+//         <h2>Trending Upwards</h2>
+//         `)}
+//     else if(stock["Weekly Time Series"][lastInput]["4. close"] < stock["Weekly Time Series"][lastWeek]["4. close"]) { 
+//         const $main = $("main")
+//             $main.append(`
+//             <h2>Trending Downwards</h2>
+//             `)}
+//      else{
+//         const $main = $("main")
+//             $main.append(`<h2>Trending Same as Previous</h2>`)
+//      }   
 
-    if(stock["Weekly Time Series"][lastInput]["4. close"] > stock["Weekly Time Series"][lastWeek]["4. close"]){ 
-    const $main = $("main")
-        //$main.empty()
-        $main.append(`
-        <h1>Trending Upwards</h1>
-        `)}
-    else if(stock["Weekly Time Series"][lastInput]["4. close"] < stock["Weekly Time Series"][lastWeek]["4. close"]) { 
-        const $main = $("main")
-            $main.append(`
-            <h1>Trending Downwards</h1>
-            `)}
-     else{
-        const $main = $("main")
-            $main.append(`<h1>Trending Same as Previous</h1>`)
-     }   
-
-})
-}
+// })
+// }
 
 
 
@@ -110,5 +160,5 @@ $("input[type=submit]").on("click", (event) => {
 
     // update the screen
     stockChecker(inputText)
-    stockTrender(inputText)
+   // stockTrender(inputText)
 })
